06998967c95c85a82c3c2e9db0ba37d0abbbb712
Marek <erbrow2000@gmail.com>
2020-05-22 22:39:58 +0200

    final commit

--- main.ts
+++ main.ts
@@ -1,66 +1,75 @@
 const express = require('express')
-import * as sqlite3 from 'sqlite3';
-
-
-const most_expensive = [
-    {
-        'id': 10,
-
-        'name': 'Gold',
-
-        'price': 1000,
-
-        'src': 'images/mem10.jpg'
-    },
-
-    {
-        'id': 9,
-
-        'name': 'Platinum',
-
-        'price': 1100,
-
-        'src': 'images/mem15.jpg'
-    },
-
-    {
-        'id': 8,
-
-        'name': 'Elite',
-
-        'price': 1200,
-
-        'src': 'images/mem13.jpg'
-    }
-
-]
 
 type memeJSON = {
-    id: number;
-    src: string;
-    price: number;
-    name: string;
+    id: number
+    price: number
+    src: string
+    name: string
 }
 
-class meme {
+class Meme {
     data: memeJSON
+    priceHistory: number[]
 
-    constructor(jsonString: string) {
-        this.data = JSON.parse(jsonString)
+    constructor(id: number, price: number, src: string, name: string) {
+        this.data = {
+            id: id,
+            price: price,
+            src: src,
+            name: name
+        }
+        this.priceHistory = []
+        this.priceHistory.push(price)
     }
 
     public change_price(price: number): void {
-
+        this.data.price = price
+        this.priceHistory.push(price)
     }
+
+    public toString(): string {
+        return JSON.stringify(this.data)
+    }
+}
+
+
+let memesArray: Meme[]
+
+function populateMemes() {
+    memesArray = []
+    for (let i = 0; i < 17; i++) {
+        memesArray.push(new Meme(i, Math.floor(Math.random() * 10000), '/images/mem' + i.toString() + '.jpg', 'TwojaStara'))
+    }
+}
+
+populateMemes()
+
+
+function most_expensive(): JSON[] {
+    let res: JSON[]
+    res = []
+    let memesCopy: Meme[] = [...memesArray]
+
+    memesCopy.sort((a: Meme, b: Meme) => {
+        return (b.data.price - a.data.price)
+    })
+
+    for (let i = 0; i < Math.min(memesCopy.length, 3); i++) {
+        res.push(JSON.parse(memesCopy[i].toString()))
+    }
+
+    return res
 }
 
 let app = express()
 app.use(express.static('public'));
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
+
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Meme market', message: 'Hello there!', memes: most_expensive })
+    res.render('index', { title: 'Meme market', message: 'Hello there!', memes: most_expensive() })
 });
 
 const port = 3000
@@ -69,27 +78,23 @@ app.listen(port, () => {
 })
 
 
-function get_meme(id: number) {
-    sqlite3.verbose()
-    let db = new sqlite3.Database('baza.db')
 
-    db.get('SELECT meme FROM data WHERE id=?', [id], (err, row) => {
-        if (err) {
-            throw err
-        }
-
-        if (row === undefined) {
-            throw (new Error('there is no meme with such id'))
-        }
-
-        return new meme(row)
-    })
+function get_meme(id: number): Meme {
+    return memesArray[id]
 }
+
+
+
+app.get('/meme/:memeId', (req, res) => {
+    let meme = get_meme(parseInt(req.params.memeId))
+    res.render('meme', { meme: JSON.parse(meme.toString()), prices: [...meme.priceHistory].reverse() })
+})
+
+
 
 app.post('/meme/:memeId', function (req, res) {
     let meme = get_meme(req.params.memeId);
     let price = req.body.price;
-    meme.change_price(price);
-    console.log(req.body.price);
-    res.render('meme', { meme: meme })
+    meme.change_price(price)
+    res.render('meme', { meme: JSON.parse(meme.toString()), prices: [...meme.priceHistory].reverse() })
 })
---
+++ memeJSON.ts
@@ -0,0 +1,6 @@
+export type memeJSON = {
+    id: number;
+    src: string;
+    price: number;
+    name: string;
+};
